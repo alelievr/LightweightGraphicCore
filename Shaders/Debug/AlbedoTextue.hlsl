87862e165a0df1a1b17742393854b4b795181db3
alelievr <alelievr@student.42.fr>
2018-11-29 00:15:56 +0100

    Finished shader compilation

--- Shaders/Debug/AlbedoTextue.hlsl
+++ Shaders/Debug/AlbedoTextue.hlsl
@@ -1,45 +1,4 @@
-struct LWGC_PerFrame
-{
-	float4		time; // x: time, y: sin(time), z: cos(time), z: deltaTime
-};
-
-struct LWGC_PerCamera
-{
-	float4x4	projection;
-	float4x4	view;
-	float4		positionWS;
-	float4		screenSize; // xy: screen size in pixel, zw: 1 / screenSize
-};
-
-struct LWGC_PerObject
-{
-	float4x4	model;
-};
-
-struct LWGC_PerMaterial
-{
-	float4		albedo;
-};
-
-[[vk::binding(0, 0)]]
-ConstantBuffer< LWGC_PerFrame >	frame;
-
-[[vk::binding(0, 1)]]
-ConstantBuffer< LWGC_PerCamera> camera;
-
-[[vk::binding(0, 2)]]
-ConstantBuffer< LWGC_PerObject > object;
-
-[[vk::binding(0, 3)]]
-ConstantBuffer< LWGC_PerMaterial > material;
-
-[[vk::binding(1, 3)]]
-uniform SamplerState trilinearClamp;
-
-[[vk::binding(2, 3)]] uniform Texture2D	albedoMap;
-[[vk::binding(3, 3)]] uniform Texture2D	normalMap;
-[[vk::binding(4, 3)]] uniform Texture2D	heightMap;
-[[vk::binding(5, 3)]] uniform Texture2D	smoothnessMap;
+#include "Shaders/Common/Uniforms.hlsl"
 
 struct FragmentOutput
 {
@@ -56,7 +15,7 @@ FragmentOutput main(FragmentInput i)
 {
 	FragmentOutput	o;
 
-	o.color = float4(albedoMap.Sample(trilinearClamp, i.uv * 2).rgb, 1.0);
+	o.color = float4(albedoMap.SampleLevel(trilinearClamp, i.uv * 2, 7).rgb, 1.0);
 
 	return o;
 }
