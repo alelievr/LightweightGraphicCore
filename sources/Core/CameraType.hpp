84764c93f9f3434f72db81290e1b8bcb0588f5c0
alelievr <alelievr@student.42.fr>
2018-11-29 14:37:15 +0100

    Renaming

--- sources/Core/CameraType.hpp
+++
@@ -1,8 +0,0 @@
-namespace LWGC
-{
-	enum class CameraType
-	{
-		Perspective,
-		Orthographic,
-	};
-}
--- sources/Utils/Rect.cpp
+++
@@ -1,9 +0,0 @@
-#include "Rect.hpp"
-
-using namespace LWGC;
-
-Rect::Rect(void)
-{
-	min = glm::vec2(0.f);
-	max = glm::vec2(0.f);
-}
