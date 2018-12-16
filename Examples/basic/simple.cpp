7e88207abc44b9f076b2483e9847447d3ba2a60b
alelievr <alelievr@student.42.fr>
2018-12-16 18:39:17 +0100

    Texture Write in shaders

--- Examples/basic/simple.cpp
+++ Examples/basic/simple.cpp
@@ -39,6 +39,8 @@ int			main(void)
 	auto	displayProceduralTexture = std::make_shared< Material >(BuiltinShaders::Standard); // Copy of the standard material
 	auto	fullScreenTest = std::make_shared< Material >(BuiltinShaders::Standard, "Shaders/FullScreenQuad.hlsl");
 
+	auto	writeTexture = std::make_shared< Material >("Shaders/Debug/TextureWrite.hlsl", "Shaders/FullScreenQuad.hlsl");
+
 	VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo = {};
 	inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
 	inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
@@ -46,36 +48,7 @@ int			main(void)
 
 	fullScreenTest->SetInputAssemblyState(inputAssemblyInfo);
 
-	VkPipelineDepthStencilStateCreateInfo depthStencilInfo = {};
-	depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-	depthStencilInfo.depthTestEnable = VK_TRUE;
-	depthStencilInfo.depthWriteEnable = VK_FALSE;
-	depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS;
-	depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
-	depthStencilInfo.stencilTestEnable = VK_FALSE;
-
-	fullScreenTest->SetDepthStencilState(depthStencilInfo);
-
-	VkPipelineRasterizationStateCreateInfo rasterizationInfo = {};
-	rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-	rasterizationInfo.depthClampEnable = VK_FALSE;
-	rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
-	rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
-	rasterizationInfo.lineWidth = 1.0f;
-	rasterizationInfo.cullMode = VK_CULL_MODE_BACK_BIT;
-	rasterizationInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
-	rasterizationInfo.depthBiasEnable = VK_FALSE;
-
-	fullScreenTest->SetRasterizationState(rasterizationInfo);
-	
-	// Temporary stuff, must be handled by the material and the render pass
-	static float test[10];
-	VkBuffer buffer;
-	VkDeviceMemory mem;
-	{
-		Vk::CreateBuffer(sizeof(test), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, buffer, mem);
-	}
-	// End Temporary stuff
+	writeTexture->SetInputAssemblyState(inputAssemblyInfo);
 
 	Texture2D jibril("images/656218.jpg", VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, true);
 
@@ -83,6 +56,7 @@ int			main(void)
 	// auto cube = new GameObject(new MeshRenderer(PrimitiveType::Cube, testMat));
 	auto cube1 = new GameObject(new MeshRenderer(PrimitiveType::Cube, displayProceduralTexture));
 	auto fullScreen = new GameObject(new ProceduralRenderer(fullScreenTest, 4));
+	auto writeTextureObject = new GameObject(new ProceduralRenderer(writeTexture, 4));
 	auto cam = new GameObject(new Camera());
 	const int workGroupSize = 8; // Work group size in the compute shader
 	// cube1->AddComponent(new ComputeDispatcher(writeProceduralTexture, 512 / workGroupSize, 512 / workGroupSize, &jibril));
@@ -99,6 +73,7 @@ int			main(void)
 	cam->AddComponent(new FreeCameraControls());
 	hierarchy->AddGameObject(cube1);
 	hierarchy->AddGameObject(fullScreen);
+	hierarchy->AddGameObject(writeTextureObject);
 	hierarchy->AddGameObject(cam);
 	// testMat->SetTexture(TextureBinding::Albedo, jibril, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE);
 
@@ -107,7 +82,10 @@ int			main(void)
 	// writeProceduralTexture->SetTexture("proceduralTexture", jibril, VK_IMAGE_LAYOUT_GENERAL, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE);
 	// "f" is inside the same descriptor set than procedural texture so we don't need to allocate his descriptor set
 	// writeProceduralTexture->SetBuffer("f", buffer, sizeof(test), VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
-	displayProceduralTexture->SetTexture(TextureBinding::Albedo, jibril, VK_IMAGE_LAYOUT_GENERAL, VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE);
+
+	writeTexture->AllocateDescriptorSet("writeTexture");
+	writeTexture->SetTexture("writeTexture", proceduralTexture, VK_IMAGE_LAYOUT_GENERAL, VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE);
+	displayProceduralTexture->SetTexture(TextureBinding::Albedo, proceduralTexture, VK_IMAGE_LAYOUT_GENERAL, VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE);
 
 	while (app.ShouldNotQuit())
 	{
